aff98f43e41ff4d0fff48b68abba27d8d8b16aa0
union-1002 <Anne.ther.u@gmail.com>
2025-05-31 15:29:17 +0000

    연혁 수정

--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -22,7 +22,7 @@ function TimelinePage() {
               </div>
 
               <div className="text-slate-500">
-                첫 게이트 각성: J / R / F / L / E / X / P / I / H / T / 비광<br/>
+                첫 게이트 각성: J / R / F / L / E / X / P / I / H / T / 비광 / 미카엘<br/>
                 번외: 라멘타 각성
               </div>
           </div>
@@ -100,7 +100,7 @@ function TimelinePage() {
             <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                 <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-22 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">5년 전 08/24</time>
                 <div className="text-xl font-book text-slate-900">
-                  데스사이트 게이트 발생
+                  데스사이드 게이트 발생
                 </div>
             </div>
 
