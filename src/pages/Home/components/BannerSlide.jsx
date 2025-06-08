90085137acb12645c314ba3e308286c087ef1cf5
union-1002 <Anne.ther.u@gmail.com>
2025-06-08 11:01:17 +0000

    배너추가

--- src/pages/Home/components/BannerSlide.jsx
+++ src/pages/Home/components/BannerSlide.jsx
@@ -3,7 +3,7 @@ import { useUser } from '@/shared/user';
 import banner1 from '../images/banner1.png';
 import banner2 from '../images/banner2.png';
 import banner3 from '../images/banner3.png';
-import banner4 from '../images/banner4.png';
+import banner7 from '../images/banner7.jpg';
 import banner5 from '../images/banner5.png';
 import banner_n from '../images/banner_n.png';
 import banner6 from '../images/banner6.jpg';
@@ -29,10 +29,10 @@ const BannerSlide = () => {
     ...(user.part === '드라칼' ? [banner_d] : []),
     ...(user.part === '헌터즈' ? [banner_hu] : []),
     banner6,
+    banner7,
     banner1,
     banner2,
     banner3,
-    banner4,
     banner5,
   ];
 
