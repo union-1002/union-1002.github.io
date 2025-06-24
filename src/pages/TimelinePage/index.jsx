cae8e5733122e8e44b5fe81c6d56ec194641bea9
union-1002 <Anne.ther.u@gmail.com>
2025-06-24 11:39:39 +0000

    연혁 추가

--- src/pages/TimelinePage/index.jsx
+++ src/pages/TimelinePage/index.jsx
@@ -22,7 +22,7 @@ function TimelinePage() {
               </div>
 
               <div className="text-slate-500">
-                첫 게이트 각성: J / R / F / L / E / X / P / I / H / T / 비광 / 미카엘<br/>
+                첫 게이트 각성: J / R / F / L / E / X / P / I / H / T / 비광 / 미카엘 / 사<br/>
                 번외: 라멘타 각성
               </div>
           </div>
@@ -45,18 +45,18 @@ function TimelinePage() {
 
           <div className="relative pl-8 sm:pl-32 pb-12 group">
 
-              <div className="font-ink font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">The Wolfdog</div>
+              <div className="font-ink font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">The 1.5 Generation</div>
 
               <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                   <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-22 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">8년 전 초봄</time>
                   <div className="text-xl font-book text-slate-900">
-                    울프독 신설
+                    1.5세대
                   </div>
               </div>
 
               <div className="text-slate-500">
                   각성: M<br/>
-                  울프독 원년 멤버: M / X
+                  울프독 원년 멤버: M / X / 비광
               </div>
           </div>
           
@@ -68,7 +68,7 @@ function TimelinePage() {
               <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                   <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-22 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">7년 전 초여름</time>
                   <div className="text-xl font-book text-slate-900">
-                    1.5세대
+                    그 애
                   </div>
               </div>
 
