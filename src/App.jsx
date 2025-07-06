9deead8d8cd4ee845dba26b6733a17ad75b07063
union-1002 <Anne.ther.u@gmail.com>
2025-07-06 15:12:53 +0000

    일단 끊어

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
         <Route path="/happybdayRco" element={<BrithdayRcoPage/>} />
         <Route path="/ranking_starrain" element={<RankPage/>} />
         <Route path="/happybdayW" element={<BirthdayWPage/>} />
-        <Route path="/happybdayM" element={<BirthdayMPage/>} />
+        {/* <Route path="/happybdayM" element={<BirthdayMPage/>} /> */}
         {/* <Route path="/happybdayBK" element={<BirthdayBPage/>} /> */}
       </Routes>
     </AppProvider>
