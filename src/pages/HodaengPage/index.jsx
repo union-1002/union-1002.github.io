07e6fda432101bace19aef87a06dfc4106c632e8
union-1002 <Anne.ther.u@gmail.com>
2025-07-24 13:51:15 +0000

    호댕 페이지 내용 임시 변경

--- src/pages/HodaengPage/index.jsx
+++ src/pages/HodaengPage/index.jsx
@@ -18,18 +18,7 @@ function HodaengPage() {
 
         </div>
         <div className='mt-5 text-center flex gap-2 justify-center'>
-          <Link
-            to="/happybdayM"
-            className="inline-block px-10 py-10 text-white/60 border border-white/60 hover:text-white hover:border-white transition rounded-md"
-          >
-            ← 리더 생일
-          </Link>
-          <Link
-            to="/happybdayBK"
-            className="inline-block px-10 py-10 text-white/60 border border-white/60 hover:text-white hover:border-white transition rounded-md"
-          >
-            비광 생일 →
-          </Link>
+          호댕 페이지
         </div>
       </div>
     </div>
