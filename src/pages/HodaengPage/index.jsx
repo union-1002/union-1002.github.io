a441386e31b5524425899d29e735c450393661a0
union-1002 <Anne.ther.u@gmail.com>
2025-08-19 11:43:27 +0000

    호댕 페이지 수정

--- src/pages/HodaengPage/index.jsx
+++ src/pages/HodaengPage/index.jsx
@@ -10,15 +10,15 @@ function HodaengPage() {
     <div className="relative w-screen h-screen overflow-hidden">
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat -z-20"
-        style={{ backgroundImage: "url('images/back_bday_W.jpg')" }}
+        style={{ backgroundImage: "url('images/hodaeng_back.png')" }}
       />
       <div className="absolute inset-0 bg-black/60 -z-10" />
       <div className="w-full h-full flex flex-col justify-center items-center overflow-auto py-10">
-        <div className="flex flex-col items-center text-center text-white">
-
-        </div>
         <div className='mt-5 text-center flex gap-2 justify-center'>
-          호댕 페이지
+          <img
+            src="images/hodaeng_main.png"
+            className="w-250 h-auto"
+          />
         </div>
       </div>
     </div>
