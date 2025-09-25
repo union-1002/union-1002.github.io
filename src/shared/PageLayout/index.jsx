4011384c0f3fc88ba6cc08c13e0c70c62b27b7c9
union-1002 <Anne.ther.u@gmail.com>
2025-09-25 13:56:58 +0000

    광고 다시

--- src/shared/PageLayout/index.jsx
+++ src/shared/PageLayout/index.jsx
@@ -3,29 +3,27 @@ import AdBottom from "../../components/AdBottom";
 
 function PageLayout({ ref, title, sidebar, children }) {
   return (
-    <div>
-      <div ref={ref} className="flex max-w-7xl mx-auto px-4 lg:px-8 mt-8 space-x-8 items-start">
-        {sidebar && <SideNavigationBar {...sidebar} />}
+    <div ref={ref} className="flex max-w-7xl mx-auto px-4 lg:px-8 mt-8 space-x-8 items-start">
+      {sidebar && <SideNavigationBar {...sidebar} />}
 
-        {/* 본문 */}
-        <div className="flex-1 flex flex-col items-center space-y-8">
+      {/* 본문 */}
+      <div className="flex-1 flex flex-col items-center space-y-8">
 
-          {/* 제목 */}
-            <div className="w-full text-left mt-8 mb-20">
-              <h1 className="text-3xl font-bold text-[#435373] mb-2">
-                {title}
-              </h1>
-              <div className="w-full h-0.5 bg-[#435373]"></div>
-            </div>
-          {/* 본문 내용 */}
-            <div className="w-full text-left px-2">
-              {children}
-            </div>
+        {/* 제목 */}
+          <div className="w-full text-left mt-8 mb-20">
+            <h1 className="text-3xl font-bold text-[#435373] mb-2">
+              {title}
+            </h1>
+            <div className="w-full h-0.5 bg-[#435373]"></div>
+          </div>
+        {/* 본문 내용 */}
+          <div className="w-full text-left px-2">
+            {children}
+          </div>
 
-            
-        </div>
+          
       </div>
-      <div className="mt-10 mb-20">
+      <div className="mt-10 mb-20 w-full">
         <AdBottom />
       </div>
     </div>
