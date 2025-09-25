39f1d59ceec0a777df9031d2e51754ab9fc2cfc1
union-1002 <Anne.ther.u@gmail.com>
2025-09-25 13:18:57 +0000

    광고 테스트2

--- src/shared/SideNavigationBar/index.jsx
+++ src/shared/SideNavigationBar/index.jsx
@@ -39,37 +39,40 @@ export const MENU_PROPS = {
 
 function SideNavigationBar({ title, links }) {
   return (
-    <div className="hidden lg:flex flex-col w-60 rounded-md border-[0.5px] border-[#435373]">
+    <div>
+      <div className="hidden lg:flex flex-col w-60 rounded-md border-[0.5px] border-[#435373]">
 
-      {/* 대제목 */}
-      <div className=" bg-[#435373] h-40 flex items-center justify-center">
-        <h2 className="text-[#ffffff] font-bold text-lg text-center">
-          {title}
-        </h2>
+        {/* 대제목 */}
+        <div className=" bg-[#435373] h-40 flex items-center justify-center">
+          <h2 className="text-[#ffffff] font-bold text-lg text-center">
+            {title}
+          </h2>
+        </div>
+
+        {/* 소메뉴 */}
+        <div className="flex flex-col divide-y divide-gray-300">
+          {Object.keys(links).map((menu, idx) => (
+            <Link
+              key={idx}
+              to={links[menu] || "/"}
+              className={`text-sm text-left font-semibold px-4 py-4 transition 
+                ${
+                  links[menu] === location.pathname
+                    ? 'border-l-4 border-[#456EBF] text-[#456EBF] rounded-md'
+                    : 'text-[#404040] hover:text-[#456EBF]'
+                }`}
+            >
+              {menu}
+            </Link>
+          ))}
+        </div>
+
+        
+
       </div>
-
-      {/* 소메뉴 */}
-      <div className="flex flex-col divide-y divide-gray-300">
-        {Object.keys(links).map((menu, idx) => (
-          <Link
-            key={idx}
-            to={links[menu] || "/"}
-            className={`text-sm text-left font-semibold px-4 py-4 transition 
-              ${
-                links[menu] === location.pathname
-                  ? 'border-l-4 border-[#456EBF] text-[#456EBF] rounded-md'
-                  : 'text-[#404040] hover:text-[#456EBF]'
-              }`}
-          >
-            {menu}
-          </Link>
-        ))}
+      <div className="mt-10">
+          <AdBanner/>
       </div>
-
-      <div>
-        <AdBanner/>
-      </div>
-
     </div>
   );
 }
