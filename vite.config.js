b1000b2bba56593e79bd4095b7f11478c4c358ad
union-1002 <Anne.ther.u@gmail.com>
2026-02-20 17:37:02 +0000

    각종 업데이트

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import eslint from "vite-plugin-eslint";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import flowbiteReact from "flowbite-react/plugin/vite";
@@ -9,7 +8,7 @@ const __dirname = path.resolve();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [eslint(), react(), tailwindcss(), flowbiteReact()],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
